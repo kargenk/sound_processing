a22f206e7a0f56bfd4d9180169f3dc23ba9dcf2a
gengen <gengen0630@gmail.com>
2019-12-15 14:10:03 +0900

    [update] DFT function

--- util.py
+++ util.py
@@ -3,6 +3,7 @@
 
 import struct
 import wave
+import numpy as np
 
 def save_wav(path, data, mono=True):
     """
@@ -27,3 +28,30 @@ def save_wav(path, data, mono=True):
         params = (channels, 2, 16000, len(binwave), 'NONE', 'not compressed')
         wav.setparams(params)
         wav.writeframes(binwave)
+
+def dft(start, x, N):
+    """
+    xのstartサンプル目からのNサンプルを周期波形とみなしたDFT値を返す
+    
+    Parameters
+    ----------
+    start : int
+        処理の開始位置
+    x : array
+        入力信号
+    N : int
+        DFTを行う点数
+    
+    returns
+    ----------
+    X : array
+        DFTの結果
+    """
+    
+    X = [0.0] * N
+    for k in range(N):
+        for n in range(N):
+            real = np.cos(2 * np.pi * k * n / N)
+            imag = - np.sin(2 * np.pi * k * n / N)
+            X[k] += x[start + n] * complex(real, imag)
+    return X
